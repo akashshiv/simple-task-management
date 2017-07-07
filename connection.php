a9b6f6e705c4fccf7111e2271044349cd2ede442
akashshiv <akashshivhare1919@gmail.com>
2017-07-07 23:04:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,23 +1,16 @@
 <?php
      session_start();
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$conn->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'assignment.sql')
-                   )
-               )
-);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], assignment.sql);
 
 
    try {
-   	 
+   	 $conn = new PDO("mysql:host=$servername;dbname=$database;",$username,$password);
    } catch (PDOException $e) {
    	  die("connection errror".$e->getMessage());
    }
