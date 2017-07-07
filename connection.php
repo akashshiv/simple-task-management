ebf7f5b8bbe61391b9661d7b0ff32eea73e2ca15
akashshiv <akashshivhare1919@gmail.com>
2017-07-07 23:20:14 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,26 +3,11 @@
 
 
 use Herrera\Pdo\PdoServiceProvider;
-use Silex\Application;
 
-$app = new Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-//print_r(array_values($dbopts));
-$app->register(new PdoServiceProvider(),
+
+$conn->register(new PdoServiceProvider(),
     array(
         'pdo.dsn' => 'mysql:dbname='.ltrim($dbopts["path"],'assignment.sql').';host='.$dbopts["host"],
         'pdo.port' => $dbopts["port"],
